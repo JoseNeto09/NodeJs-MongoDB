9ff652697b8b9ef891a787c8f395991d6f62b7ba
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-09-19 15:55:14 -0300

    Criando Conexão com o mysql e recuperando dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,20 @@
 module.exports = function(app){
+
     app.get('/noticias', function(req, res) {
-        res.render("noticias/noticias");
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'portal_noticias'
+
+        });
+
+        connection.query('select * from noticias', function (erro, results){
+            res.send(results);
+        });
+        
+        //res.render("noticias/noticias");
     });
-}
+};
